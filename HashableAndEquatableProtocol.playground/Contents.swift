b14743d4adbedd63ac9f13edbaf653686b3b2a1c
Virendra Gupta <vgupta@ivycomptech.com>
2020-01-08 00:08:05 +0530

    second  commit

--- HashableAndEquatableProtocol.playground/Contents.swift
+++ HashableAndEquatableProtocol.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
+struct SimpleStruct: Hashable {
+    
+    var identifier: String
+    
+}
+
+
+var array = [SimpleStruct]()
+array.append(SimpleStruct(identifier: "id"))
+
+var set = Set<SimpleStruct>()
 
